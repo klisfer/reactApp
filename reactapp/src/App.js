06ec94f108f161f564a9e2a26a3880951660c49d
klisfer1 <aayush@mycaptain.in>
2018-08-22 11:56:30 +0530

    second commit my captain app

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -5,15 +5,37 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <div>
+      <hgroup>
+        <h1>Welcome to The Climber</h1>
+        <h3>By Aayush Chaubey</h3>
+      </hgroup>
+      <form>
+        <div class="group">
+          <input type="email" />
+          <span class="highlight" />
+          <span class="bar" />
+          <label>Email</label>
+        </div>
+        <div class="group">
+          <input type="password" />
+          <span class="highlight" />
+          <span class="bar" />
+          <label>Password</label>
+        </div>
+        <button type="button" class="button buttonBlue">
+          Login
+          <div class="ripples buttonRipples">
+            <span class="ripplesCircle" />
+          </div>
+        </button>
+      </form>
+      <footer>
+        <a href="http://www.polymer-project.org/" target="_blank">
+          <img src="https://www.polymer-project.org/images/logos/p-logo.svg" />
+        </a>
+      </footer>
+    </div>
     );
   }
 }
